b2316711093bbbc18b21f2e717ccf22c9f8ef1f9
Andy54678 <151685258+Andy54678@users.noreply.github.com>
2024-02-04 18:34:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var images = "http://www.freeiconspng.com/uploads/father-son-dad-and-baby-png-5.png,https://tse4.explicit.bing.net/th?id=OIP.v_tEbgrOgWStIFqfMSi0dQHaHa&pid=Api&P=0&h=180,https://tse2.mm.bing.net/th?id=OIP.W7X345VZOFeg9u578-vp7gHaE_&pid=Api&P=0&h=180,https://www.pngitem.com/pimgs/m/521-5211656_cute-cartoon-characters-boy-hd-png-download.png";
+var name = "Luis Claudio Santana,Rejane Ferreira da Silveira,Otavio Silveira Santana, Heitor Silveira Santana";
+var i = 0
+function updates(){
+    i++
+    var numbersOfFamilyInArray = 4
+    if(i > numbersOfFamilyInArray){
+        i = 0
+    }
+}
